66e9c7081449f6e74510883635d45515d50ddd5e
ananyacbt <ananyachakraborty028@gmail.com>
2025-09-22 20:53:04 +0530

    third commit

--- src/langgraphagenticai/UI/streamlitui/load.py
+++ src/langgraphagenticai/UI/streamlitui/load.py
@@ -26,12 +26,20 @@ class LoadStreamlitUI:
                 # Model selection
                 model_options = self.config.get_groq_model_options()
                 self.user_controls["selected_groq_model"] = st.selectbox("Select Model", model_options)
-                self.user_controls["GROQ_API_KEY"] = st.session_state["GROQ_API_KEY"]=st.text_input("API Key",type="password")
+                self.user_controls["GROQ_API_KEY"] = st.session_state["GROQ_API_KEY"]=st.text_input("Groq API Key",type="password")
                 # Validate API key
                 if not self.user_controls["GROQ_API_KEY"]:
                     st.warning("⚠️ Please enter your GROQ API key to proceed. Don't have? refer : https://console.groq.com/keys ")
             
             ## USecase selection
             self.user_controls["selected_usecase"]=st.selectbox("Select Usecases",usecase_options)
+
+            if self.user_controls["selected_usecase"] == 'Chatbot With A Web Search Tool':
+                os.environ["TAVILY_API_KEY"]=self.user_controls["TAVILY_API_KEY"] = st.session_state["TAVILY_API_KEY"]=st.text_input("Tavily API Key",type="password")
+                # Validate API key
+                if not self.user_controls["TAVILY_API_KEY"]:
+                    st.warning("⚠️ Please enter your TAVILY API key to proceed. Don't have? refer : https://app.tavily.com/ ")
+            
+
 
         return self.user_controls
